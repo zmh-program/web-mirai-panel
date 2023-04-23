f60fb87324fa663f21eb4dbd58359c563727a4cb
Marcos Rogers <88774611+Cloxl@users.noreply.github.com>
2023-04-23 08:44:19 +0800

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,85 +1,73 @@
-from gevent import monkey
-monkey.patch_all()
-
-from flask import Flask, request, jsonify
-from flask_socketio import SocketIO, emit
-import cli
-import file_transfer
 import os
 import toml
+import time
+from gevent import monkey
+from gevent.pywsgi import WSGIServer
+from flask import Flask, request, jsonify
+from flask_socketio import SocketIO, emit
+from functions import execute_command, save_config_commond, handle_upload
+from docker import DockerClient
+
+monkey.patch_all()
+client = DockerClient(base_url='unix://var/run/docker.sock')
 
 app = Flask(__name__)
 socketio = SocketIO(app, async_mode='gevent')
 
-'''接收需要执行的命令'''
-@app.route('/configeditor/api/command', methods=['POST'])
+'''处理命令'''
+@app.route('/api/command', methods=['POST'])
 def handle_command_input():
     command = request.json['command']
     socketio.emit('command_input', command)
-    return jsonify({'执行命令状态': '成功'})
-
-'''使用cli.py运行命令'''
+    return jsonify({'status': True})
 @socketio.on('command_input')
 def handle_command_input_socket(command):
-    output_generator = cli.execute_command(command)
+    output_generator = execute_command(command)
     for output in output_generator:
         emit('command_output', output)
 
-'''处理上传文件命令'''
-@app.route('/configeditor/api/upload', methods=['POST'])
-def handle_upload():
+'''上传文件'''
+@app.route('/api/upload', methods=['POST'])
+def handle_uploaded_file():
     file = request.files['file']
-    file_transfer.handle_upload(file)
-    return jsonify({'上传配置文件状态': '成功'})
+    handle_upload(file)
+    return jsonify({'status': True})
 
-'''读取配置文件'''
+'''加载文件'''
 @app.route('/api/loadconfig', methods=['POST'])
 def load_config():
-    config_path = find_config_file()
-    if config_path:
+    config_path = 'config.cfg'
+    if os.path.exists(config_path):
         with open(config_path, 'r') as file:
             config_data = toml.load(file)
         return jsonify(config_data)
     else:
-        return jsonify({'读取配置文件状态': '异常'})
+        return jsonify({'status': False})
 
-'''辅助函数 寻找有没有config.cfg文件'''
-def find_config_file():
-    config_filename = 'config.cfg'
-    chatgpt_directory = 'chatgpt-qq'
-    if os.path.exists(config_filename):
-        return config_filename
-    elif os.path.exists(chatgpt_directory) and os.path.exists(os.path.join(chatgpt_directory, config_filename)):
-        return os.path.join(chatgpt_directory, config_filename)
-    else:
-        return None
-
-'''保存配置文件'''
+'''保存文件'''
 @app.route('/api/saveconfig', methods=['POST'])
 def save_config():
-    config_data = request.json
-    config_path = find_docker_compose_file()
-    if config_path:
-        config_dir = os.path.dirname(config_path)
-        config_file_path = os.path.join(config_dir, 'config.cfg')
-        
-        with open(config_file_path, 'w') as file:
-            toml.dump(config_data, file)
-        return jsonify({'保存配置文件状态': '成功'})
-    else:
-        return jsonify({'保存配置文件状态': '异常'})
-        
-'''辅助函数 寻找有没有docker-compose.yaml文件'''
-def find_docker_compose_file():
-    docker_compose_filename = 'docker-compose.yaml'
-    chatgpt_directory = 'chatgpt-qq'
-    if os.path.exists(docker_compose_filename):
-        return docker_compose_filename
-    elif os.path.exists(chatgpt_directory) and os.path.exists(os.path.join(chatgpt_directory, docker_compose_filename)):
-        return os.path.join(chatgpt_directory, docker_compose_filename)
-    else:
-        return None
+    return functions.save_config_commond()
+'''备用节点'''
+@app.route('/api/saveconfig1', methods=['POST'])
+def save_config1():
+    return functions.save_config_commond()
 
+
+'''容器运行状态'''
+@app.route('/api/containerstatus', methods=['GET'])
+def get_container_status():
+    containers = client.containers.list(all=True)
+    container_statuses = []
+
+    for container in containers:
+        container_statuses.append({
+            'id': container.short_id,
+            'name': container.name,
+            'status': container.status
+        })
+
+    return jsonify(container_statuses)
 
 if __name__ == '__main__':
     socketio.run(app)
