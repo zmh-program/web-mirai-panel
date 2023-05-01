ad90b080a37391f27f0f3a8d37f366176a2482fa
Zhang Minghan <zmh13054618081@dingtalk.com>
2023-05-01 19:28:32 +0800

    remove icon

--- frontend/components.d.ts
+++ frontend/components.d.ts
@@ -21,7 +21,6 @@ declare module '@vue/runtime-core' {
     ElForm: typeof import('element-plus/es')['ElForm']
     ElFormItem: typeof import('element-plus/es')['ElFormItem']
     ElHeader: typeof import('element-plus/es')['ElHeader']
-    ElIcon: typeof import('element-plus/es')['ElIcon']
     ElInput: typeof import('element-plus/es')['ElInput']
     ElInputNumber: typeof import('element-plus/es')['ElInputNumber']
     ElLink: typeof import('element-plus/es')['ElLink']
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,7 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import ElementPlus from 'element-plus'
-import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import './assets/style/main.css'
 
 const app = createApp(App)
@@ -10,4 +9,3 @@ const app = createApp(App)
 app.use(router)
 app.use(ElementPlus)
 app.mount('#app')
-for (const [key, component] of Object.entries(ElementPlusIconsVue)) app.component(key, component)
