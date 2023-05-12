188bdca033b50bc2997bfeb49144e6317a1c90ae
Zhang Minghan <zmh13054618081@dingtalk.com>
2023-05-13 00:15:31 +0800

    add console

--- frontend/src/assets/script/socket.ts
+++ frontend/src/assets/script/socket.ts
@@ -1,9 +1,5 @@
 import { io } from 'socket.io-client';
 
-export const socket = io(`ws://${location.host}/api/command`, {
+export const socket = io(`ws://${location.host}`, {
   reconnectionDelayMax: 10000,
 })
-
-socket.on("connect", () => {
-  console.log(socket.connected); // true
-});
--- frontend/src/components/ShellCommand.vue
+++ frontend/src/components/ShellCommand.vue
@@ -1,12 +1,40 @@
 <script setup lang='ts'>
 import { socket } from '@/assets/script/socket'
+import { ref } from 'vue'
+import type { Ref } from 'vue'
+
+const buffer: Ref<string[]> = ref([]);
+const input: Ref<string> = ref("");
+
+function submit() {
+  const data: string = input.value;
+  if (data) socket.emit("command_input", data);
+}
+
+socket.on("command_output", function(data: string) {
+  return buffer.value.push(data);
+})
 
 </script>
 
 <template>
-
+  <div class='console'>
+    <div v-for='(data, idx) in buffer' :key='idx'>{{ data }}</div>
+  </div><br>
+  <el-form :inline='true'>
+    <el-form-item style='width: calc(100% - 72px); margin-right: 12px'><el-input v-model='input' /></el-form-item>
+    <el-form-item style='width: 60px; margin-right: 0'><el-button type='primary' @click='submit'>发送</el-button></el-form-item>
+  </el-form>
 </template>
 
 <style scoped>
-
+.console {
+    height: max-content;
+    min-height: 240px;
+    width: 100%;
+    color: #fff;
+    background: #181818;
+    padding: 12px;
+    line-height: 20px;
+}
 </style>
