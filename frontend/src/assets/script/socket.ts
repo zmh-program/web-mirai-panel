526a8dfdade888468de734727956e25c31a72f12
Zhang Minghan <zmh13054618081@dingtalk.com>
2023-05-13 09:13:53 +0800

    add buffer size & notification

--- frontend/src/assets/script/socket.ts
+++ frontend/src/assets/script/socket.ts
@@ -1,5 +1,12 @@
 import { io } from 'socket.io-client';
+import { ElNotification } from 'element-plus'
 
 export const socket = io(`ws://${location.host}`, {
   reconnectionDelayMax: 10000,
 })
+
+socket.on("connect", () => ElNotification({
+  title: "连接成功",
+  message: "服务器连接成功！",
+  type: "success"
+}))
--- frontend/src/components/ShellCommand.vue
+++ frontend/src/components/ShellCommand.vue
@@ -6,12 +6,18 @@ import type { Ref } from 'vue'
 const buffer: Ref<string[]> = ref([]);
 const input: Ref<string> = ref("");
 
+
+function listener(ev: KeyboardEvent): void {
+  if (ev.key === "Enter") submit();
+}
 function submit() {
   const data: string = input.value;
   if (data) socket.emit("command_input", data);
+  input.value = "";
 }
 
 socket.on("command_output", function(data: string) {
+  if (buffer.value.length > 25) buffer.value.shift();
   return buffer.value.push(data);
 })
 
@@ -19,22 +25,33 @@ socket.on("command_output", function(data: string) {
 
 <template>
   <div class='console'>
-    <div v-for='(data, idx) in buffer' :key='idx'>{{ data }}</div>
+    <span v-for='(data, idx) in buffer' :key='idx'>{{ data }}</span>
   </div><br>
   <el-form :inline='true'>
     <el-form-item style='width: calc(100% - 72px); margin-right: 12px'><el-input v-model='input' /></el-form-item>
-    <el-form-item style='width: 60px; margin-right: 0'><el-button type='primary' @click='submit'>发送</el-button></el-form-item>
+    <el-form-item style='width: 60px; margin-right: 0'><el-button type='primary' @click='submit' @keyup='listener'>发送</el-button></el-form-item>
   </el-form>
 </template>
 
 <style scoped>
+@keyframes fadeInAnimation {
+    0% { opacity: 0 }
+    100% { opacity: 1 }
+}
+
 .console {
     height: max-content;
     min-height: 240px;
     width: 100%;
     color: #fff;
     background: #181818;
-    padding: 12px;
-    line-height: 20px;
+    padding: 16px;
+    font-size: 12px;
+    font-family: 'Open Sans', sans-serif;
+    letter-spacing: 0.01cm;
+}
+
+.console span {
+    animation: fadeInAnimation .5s;
 }
 </style>
