b6aaad38e70ac77bc391646c6018d29b85fa3d0e
Cloxl <88774611+Cloxl@users.noreply.github.com>
2023-05-18 13:15:36 +0800

    fix handle upload function

--- utils/upload_file.py
+++ utils/upload_file.py
@@ -1,6 +1,13 @@
 import os
 
-def handle_upload(file) -> None: 
-     if not os.path.exists(UPLOAD_FOLDER): 
-         os.makedirs(UPLOAD_FOLDER) 
-     file.save(os.path.join(UPLOAD_FOLDER, file.filename))
+def handle_upload(file) -> str:
+    try:
+        if file.filename == 'session.token':
+            save_folder = os.path.join(os.getcwd(), 'gocqhttp')
+        else:
+            save_folder = os.path.join(os.getcwd(), 'upload')
+            
+        if not os.path.exists(save_folder):
+            os.makedirs(save_folder)
+            
+        file.save(os.path.join(save_folder, file.filename))
