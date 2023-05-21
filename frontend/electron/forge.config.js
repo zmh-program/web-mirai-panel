872e8632d5882130ec4e1496609382a3c8a004e8
Zhang Minghan <zmh13054618081@dingtalk.com>
2023-05-21 20:44:58 +0800

    workflow auto publish

--- frontend/electron/forge.config.js
+++ frontend/electron/forge.config.js
@@ -5,4 +5,17 @@ module.exports = {
     name: '@electron-forge/maker-squirrel',
     platforms: ['win32'],
   }],
+  publishers: [
+    {
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: 'zmh-program',
+          name: 'web-chatgpt-qq-bot',
+        },
+        prerelease: false,
+        draft: false,
+      },
+    },
+  ],
 }
