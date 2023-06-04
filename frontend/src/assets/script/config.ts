1ba8432479d4d6e2761fe5347be1ec291bfa7959
Zhang Minghan <zmh13054618081@dingtalk.com>
2023-06-04 14:18:03 +0800

    fix top-level async error

--- frontend/src/assets/script/config.ts
+++ frontend/src/assets/script/config.ts
@@ -1,15 +1,19 @@
-import { reactive, ref, watch } from 'vue'
 import type { Ref } from 'vue'
+import { reactive, ref, watch } from 'vue'
 import axios from 'axios'
 import { authenticated } from '@/assets/script/utils'
 
 
-export const isAuthenticated = ref(await authenticated(localStorage.getItem("auth") || ""));
+export const isAuthenticated = ref(false);
 export const collapse: Ref<string[]> = ref(['chat', 'ai']);
 export const settings = reactive({
   password: localStorage.getItem("auth") || "",
   background: "https://s1.ax1x.com/2023/05/22/p9okyfs.jpg",
 })
+
+setTimeout(async () => {
+  isAuthenticated.value = await authenticated(settings.password);
+}, 0)
 
 axios.defaults.headers.common['auth'] = settings.password;
 
